3881c2d3639ff89043802144a2e80322f5753564
bryzey2019 <bryzey2019@gmail.com>
2020-02-05 09:36:18 +1100

    completed looping a triangle challenge using a for loop

---
+++ looping-a-triangle.js
@@ -0,0 +1,21 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+let abc = "abc";
+console.log(abc.length);
+// → 3
+Most exercises contain a piece of code that you can modify to solve the exercise. Remember that you can click code blocks to edit them.
+*/
+
+for (let counter = "#"; counter.length <= 7; counter += "#") {
+  console.log(counter);
+}
