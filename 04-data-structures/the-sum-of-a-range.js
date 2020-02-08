59f632937968d0b8f275576c68f67128cc61f77a
bryzey2019 <bryzey2019@gmail.com>
2020-02-08 12:36:29 +1100

    completed exercise using for loops

---
+++ 04-data-structures/the-sum-of-a-range.js
@@ -0,0 +1,24 @@
+// Your code here.
+function range(start, end) {
+  let result = [];
+
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+function sum(arr) {
+  let count = 0;
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] >= 0) {
+      count += arr[i];
+    }
+  }
+  return count;
+}
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55
